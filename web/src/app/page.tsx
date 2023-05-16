698b1c7e5e3e8333825c636a621ccbd0e81b18cd
Nickolas Rodrigues <nickolas_rodrigues@KMB_SP>
2023-05-16 17:58:16 -0300

    Primeiro commit

---
+++ web/src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/Button'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Button title="Click me diferente" />
+      <Button title="Click me diferente" />
+      <Button title="Click me diferente" />
+      <Button title="Click me diferente" />
+      <Button title="Click me diferente" />
+      <Button title="Click me diferente" />
+      <Button title="Click me diferente" />
+    </div>
+  )
+}
